9ae39ba453f6582923be192412cdaca8c8c8a9f1
berru-g <g.leberruyer@gmail.com>
2023-03-22 16:37:08 +0100

    def price test

--- init.py
+++ init.py
@@ -24,11 +24,11 @@ product_label.config(bg="#1d3557")
 product_entry = tk.Entry(root)
 product_entry.grid(row=0, column=1, padx=10, pady=10)
 
-location_label = tk.Label(root, text="Recherche Produit", foreground="#f1faee")
-location_label.grid(row=0, column=0, padx=10, pady=10)
-location_label.config(bg="#1d3557")
-location_entry = tk.Entry(root)
-location_entry.grid(row=1, column=1, padx=10, pady=10)
+prix_label = tk.Label(root, text="Recherche Produit", foreground="#f1faee")
+prix_label.grid(row=0, column=0, padx=10, pady=10)
+prix_label.config(bg="#1d3557")
+prix_entry = tk.Entry(root)
+prix_entry.grid(row=1, column=1, padx=10, pady=10)
 
 results_text = tk.Text(root)
 font_style = ("Helvetica", 12)
@@ -37,11 +37,11 @@ results_text.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
 def search():
     product = product_entry.get()
-    location = location_entry.get()
+    prix = prix_entry.get()
     
-    #https://www.amazon.fr/s?k=arduino+component&language=fr_FR&crid=AQBXL9USLKCE&linkCode=sl2&linkId=1ab2135daf6e0bded135d63b11fbba40&sprefix=%2Caps%2C290&tag=makeandplay24-21&ref=as_li_ss_tl# search firstsite.com  # bug de la requete 'location', essaie d'inverser lieux et motcles comme dans lurl dorigine, puis change l'ordre de format, product, location= sans succes
-    firstsite_url = "https://www.amazon.fr/s?k={}&language=fr_FR&tag=makeandplay24-21" # 'location' not taken into account. because not "=" in url? 
-    firstsite_search = requests.get(firstsite_url.format(product,location))
+    #https://www.amazon.fr/s?k={}+component&language=fr_FR&crid=AQBXL9USLKCE&linkCode=sl2&linkId=1ab2135daf6e0bded135d63b11fbba40&sprefix=%2Caps%2C290&tag=makeandplay24-21&ref=as_li_ss_tl# search firstsite.com  # bug de la requete 'prix', essaie d'inverser lieux et motcles comme dans lurl dorigine, puis change l'ordre de format, product, prix= sans succes
+    firstsite_url = "https://www.amazon.fr/s?k={}" # 'prix' not taken into account. because not "=" in url? 
+    firstsite_search = requests.get(firstsite_url.format(product))
     firstsite_soup = BeautifulSoup(firstsite_search.text, "html.parser")
     firstsite_titles = firstsite_soup.find_all(class_="a-size-base-plus a-color-base a-text-normal")
     subtitles = firstsite_soup.find_all(class_="a-size-mini a-spacing-none a-color-base s-line-clamp-2")
@@ -60,7 +60,8 @@ def search():
     results_text.tag_config("subtitle", foreground=subtitle_color)
     results_text.tag_config("price", foreground=price_color)
     print("resultat first site")
-    time.sleep(1) 
+    time.sleep(0.5)
+     
     # Search secondsite.com
     secondsite_url ="https://fr.aliexpress.com/w/wholesale-{}.html?&SearchText={}"               
     secondsite_search = requests.get(secondsite_url.format(product,product))
@@ -77,10 +78,11 @@ def search():
         results_text.insert(tk.END, f"{price.text}\n", "price")
         results_text.insert(tk.END, "__________\n")
     print("resultat second site")
-    time.sleep(1)   
+    time.sleep(0.5)   
+    
     # Search thirdsite.com
     thirdsite_url ="https://www.ebay.fr/sch/i.html?&_nkw={}&_sacat=0"               
-    thirdsite_search = requests.get(thirdsite_url.format(product,location))
+    thirdsite_search = requests.get(thirdsite_url.format(product,prix))
     thirdsite_soup = BeautifulSoup(thirdsite_search.text, "html.parser")
     thirdsite_titles = thirdsite_soup.find_all(class_="s-item__title")
     thirdsite_subtitles = thirdsite_soup.find_all(class_="s-item__shipping s-item__logisticsCost")
@@ -94,23 +96,42 @@ def search():
         results_text.insert(tk.END, f"{price.text}\n", "price")
         results_text.insert(tk.END, "__________\n")
     print("resultat third site")
-    time.sleep(1) 
+    time.sleep(0.5) 
       
 print("Fin du scrap")
 
-"""# def compare price 
-def get_cheapest_price(soup):
-    prices = soup.select('.price')
-    if prices:
-        # Trie les prix par ordre croissant et renvoie le premier
-        return sorted(prices, key=lambda x: float(x.text))[0]
-    return None
-cheapest_price = get_cheapest_price(firstsite_soup)
-if cheapest_price:
-    print('Le prix le moins cher est:', cheapest_price.text)
-else:
-    print('Aucun prix n\'a été trouvé')
 """
+def compare():
+    secondsite_url ="https://fr.aliexpress.com/w/wholesale-{}.html?&SearchText={}"               
+    secondsite_search = requests.get(secondsite_url.format(product_entry,product_entry))
+    secondsite_soup = BeautifulSoup(secondsite_search.text, "html.parser")
+    secondsite_titles = secondsite_soup.find_all(class_="manhattan--titleText--WccSjUS")
+    secondsite_subtitles = secondsite_soup.find_all(class_="manhattan--trade--2PeJIEB")#md:tw-text-xlOld tw-text-2xlOld tw-leading-[1.625rem]
+    secondsite_prices = secondsite_soup.find_all(class_="manhattan--price-sale--1CCSZfK") 
+    response = requests.get(secondsite_url)
+    # Trouver tous les éléments avec la balise "price"
+    prices = firstsite_soup.find_all('price')
+    # Initialiser le prix le plus bas à None
+    lowest_price = None
+    # Parcourir tous les prix trouvés
+    for price in prices:
+        # Extraire le contenu du prix (en supposant que ce soit un nombre)
+        price_value = float(price.text.strip())
+            # Si le prix le plus bas n'a pas encore été initialisé
+        if lowest_price is None:
+            lowest_price = price_value
+            # Sinon, si le prix actuel est plus bas que le prix le plus bas trouvé jusqu'à présent
+        elif price_value < lowest_price:
+            lowest_price = price_value
+            # Afficher le prix le plus bas trouvé
+        print('Le prix le plus bas est:', lowest_price)
+        # Analyser le contenu HTML de la réponse avec BeautifulSoup
+        firstsite_soup = BeautifulSoup(response.text, 'html.parser')
+        for title, subtitle, price in zip(secondsite_prices):
+            results_text.insert(tk.END, f"{lowest_price.text}\n", "price")
+"""
+
+print("saut de fonction")        
 search_button = tk.Button(root, text="Rechercher", command=search)
 search_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
